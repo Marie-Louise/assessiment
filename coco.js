c3196b20ddbea706fcf08dcbad3109d25067d8b3
Marie-Louise <mariemuteteyimana@gmail.com>
2021-06-29 19:26:29 +0300

    first commit

---
+++ coco.js
@@ -0,0 +1,17 @@
+function name(){
+    var firstNumber="hello my mum I miss you and I love you"
+    var secondNumber=34 
+    console.log(firstNumber+secondNumber)
+}
+name(name)
+
+person={
+firstName:"Marie Louise",
+nationality:"Rwandan",
+age:20,
+
+
+}
+console.log(person.firstName)
+console.log(person.nationality)
+console.log(person.age)
